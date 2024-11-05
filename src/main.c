8551db062a85f2c216310a6c03340e3c633058a0
Joao Danilo Correia Jota <joaodanilo2006@gmail.com>
2024-11-05 16:56:27 -0300

    labirinto

--- src/main.c
+++ src/main.c
@@ -1,40 +1,78 @@
-/**
- * main.h
- * Created on Oct, 29th 2024
- * Author: Célio Dias, João Danilo Jota e Pedro Caldas
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "keyboard.h"
 #include "screen.h"
 #include "timer.h"
 
-// Definição do personagem
 #define PLAYER '@'
 #define INITIAL_X 40
 #define INITIAL_Y 12
+
+#define MAZE_WIDTH (MAXX - 2)
+#define MAZE_HEIGHT (MAXY - 2)
+
+char maze[MAZE_HEIGHT][MAZE_WIDTH + 1];
+char maze2[MAZE_HEIGHT][MAZE_WIDTH + 1];
 
 // Definindo a posição do personagem
 int playerX = INITIAL_X;
 int playerY = INITIAL_Y;
 
+// Ponteiro para o labirinto atual
+char (*currentMaze)[MAZE_WIDTH + 1];
+
+void drawMaze(char maze[MAZE_HEIGHT][MAZE_WIDTH + 1]);
+void generateMaze(char maze[MAZE_HEIGHT][MAZE_WIDTH + 1]);
+void movePlayer(char direction);
 void drawPlayer();
 void clearPlayer();
-void movePlayer(char direction);
+
+void generateMaze(char maze[MAZE_HEIGHT][MAZE_WIDTH + 1]) {
+    // Gerar um labirinto aleatório simples
+    for (int y = 0; y < MAZE_HEIGHT; y++) {
+        for (int x = 0; x < MAZE_WIDTH; x++) {
+            // Define paredes e caminhos
+            if (rand() % 5 == 0) {
+                maze[y][x] = '#'; // Paredes
+            } else {
+                maze[y][x] = ' '; // Caminhos
+            }
+        }
+    }
+
+    // Adicionando o símbolo '%' como a porta para o segundo labirinto
+    maze[MAZE_HEIGHT / 2][MAZE_WIDTH / 2] = '%'; // Colocando a porta no meio
+    maze[MAZE_HEIGHT][MAZE_WIDTH] = '\0'; // Finalizando a string
+}
+
+void drawMaze(char maze[MAZE_HEIGHT][MAZE_WIDTH + 1]) {
+    for (int y = 0; y < MAZE_HEIGHT; y++) {
+        for (int x = 0; x < MAZE_WIDTH; x++) {
+            screenGotoxy(x + 1, y + 1); // Ajustando para não "comer" as bordas
+            printf("%c", maze[y][x]);
+        }
+    }
+    screenUpdate();
+}
 
 int main() {
     // Inicializando componentes
     keyboardInit();
     screenInit(1);
     timerInit(100);
-
-    // Desenhando o personagem inicial
+    
+    // Gerando o labirinto
+    generateMaze(maze);
+    drawMaze(maze);
     drawPlayer();
+    
+    // Define o labirinto inicial
+    currentMaze = maze;
 
     char key;
     int running = 1;
-    
+
     // Loop principal
     while (running) {
         if (keyhit()) {
@@ -43,6 +81,17 @@ int main() {
                 running = 0;
             } else {
                 movePlayer(key);
+                // Verifica se o jogador atingiu o símbolo '%'
+                if (currentMaze[playerY - 1][playerX - 1] == '%') {
+                    // Muda para o segundo labirinto
+                    printf("Você entrou em um novo labirinto!\n");
+                    generateMaze(maze2); // Gera um novo labirinto
+                    drawMaze(maze2); // Desenha o novo labirinto
+                    currentMaze = maze2; // Aponta para o novo labirinto
+                    playerX = INITIAL_X; // Reinicia a posição do jogador
+                    playerY = INITIAL_Y; // Reinicia a posição do jogador
+                    drawPlayer(); // Desenha o jogador na nova posição
+                }
             }
         }
     }
@@ -68,11 +117,24 @@ void clearPlayer() {
 
 void movePlayer(char direction) {
     clearPlayer();
+    int newX = playerX;
+    int newY = playerY;
+
     switch (direction) {
-        case 'w': if (playerY > MINY + 1) playerY--; break;
-        case 's': if (playerY < MAXY - 1) playerY++; break;
-        case 'a': if (playerX > MINX + 1) playerX--; break;
-        case 'd': if (playerX < MAXX - 2) playerX++; break;
+        case 'w': newY--; break; // Para cima
+        case 's': newY++; break; // Para baixo
+        case 'a': newX--; break; // Para esquerda
+        case 'd': newX++; break; // Para direita
     }
+
+    // Ajuste para a verificação de colisão no labirinto atual
+    if (newX >= 1 && newX <= MAZE_WIDTH && newY >= 1 && newY <= MAZE_HEIGHT) {
+        if (currentMaze[newY - 1][newX - 1] != '#') {
+            // Movimento válido
+            playerX = newX;
+            playerY = newY;
+        }
+    }
+
     drawPlayer();
 }
